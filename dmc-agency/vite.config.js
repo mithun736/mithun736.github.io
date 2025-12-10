df72e060c62b509416868e213ac263451bfeaf2e
mithun736 <mithunrohith@gmail.com>
2025-12-10 10:18:08 +0530

    commit

--- dmc-agency/vite.config.js
+++ dmc-agency/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dmc-agency/",
+  base: "/mithun736.github.io/",
   plugins: [react()],
 });
